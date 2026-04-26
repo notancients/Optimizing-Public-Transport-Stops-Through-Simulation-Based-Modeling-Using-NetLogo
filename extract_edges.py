8f7b12e361809c7ef9a5dac7ac7a09fdb94fb6c6
Erjoy Constantine L. Robles <elrobles1@up.edu.ph>
2026-04-27 06:57:23 +0800

    refactor: modified categorize buildings

--- extract_edges.py
+++ extract_edges.py
@@ -17,10 +17,14 @@ def fetch_roads(place_name, world_size):
     # 'all' fetches drivable roads AND pedestrian walkways/paths
     G = ox.graph_from_place(place_name, network_type='all', simplify=True)
     nodes, edges = ox.graph_to_gdfs(G)
+    edges.to_csv("raw_edges.csv", index=False)
+    nodes.to_csv("raw_nodes.csv", index=False)
+
     # Step 3:
     # Apply proper formatting and scale it so that it works in the size of our NetLogo world
     print("Formatting nodes.")
     nodes_df = nodes.reset_index()[['osmid', 'x', 'y']]
+
     nodes_df.rename(columns={'osmid': 'node_id', 'x': 'x_coord', 'y': 'y_coord'}, inplace=True)
 
     # We use the min/max of the nodes to establish the boundary of our NetLogo world
@@ -60,27 +64,90 @@ def fetch_roads(place_name, world_size):
     }
 
 
-# Categorize each building
+SPECIAL = ["weather_station", "shrine", "civic", "chapel", "veterinary", "townhall", "prison", "place_of_worship", "laboratory", "fire_station", "police", "veterinary", "dentist", "doctors", "studio", "clinic", "childcare", "courthouse", "gravey_yard", "health_post", "hospital", ]
+SCHOOL = ["university", "school", "research_institute", "prep_school", "library", "lecture_hall", "kindergarten", "college"]
+TRANSPORT = ["train_station", "pedicab_terminal", "taxi", "pedicab", "terminal", "bicycle_rental", "bus_station"]
+COMMERCIAL = ["supermarket", "retail", "office", "industrial", "garages", "garage", "commercial", "clubhouse", "theatre", "restaurant", "money_transfer", "marketplace", "karaoke_box", "internet_cafe", "ice_cream", "bar", "car_wash", "events_venue", "food_court", "fuel", "fast_food", "pharmacy", "bank", "parking", "atm", "telephone", "ice_cream" "pub", "cafe", "gambling", "bureau_de_change", ]
+MISCELLANEOUS = ["public", "greenhouse", "guardhouse", "farm_auxiliary", "waste_basket", "vending_machine", "telephone", "social_facility", "security_booth", "recycling", "post_office", "post_box", "parking_space", "parking", "motorcycle_tax", "motorcycle_parking", "hut", "community_centre", "conference_centre", "computer","toilets", "bench", "waste_basket", "fountain", "auditorium", "air_filling", "bicycle_parking"]
+RESIDENTIAL = ["residential", "house", "detached", "dormitory", "shelter", 'apartments']
+
+def build_category_map():
+    category_map = {}
+    groups = {
+        'special': SPECIAL,
+        'school': SCHOOL,
+        'transport': TRANSPORT,
+        'commercial': COMMERCIAL,
+        'miscellaneous': MISCELLANEOUS,
+        'residential': RESIDENTIAL,
+    }
+    
+    for category, values in groups.items():
+        for v in values:
+            category_map[v] = category
+    return category_map
+
+CATEGORY_MAP = build_category_map()
+
 def categorize_building(row):
     amenity = str(row.get('amenity', '')).lower()
-    b_type = str(row.get('building', '')).lower()
     
-    if 'school' in amenity or 'college' in amenity or 'university' in amenity:
-        return 'school'
-    elif 'hospital' in amenity or 'clinic' in amenity:
-        return 'special' # Special zones
-    elif b_type in ['retail', 'commercial', 'supermarket', 'office']:
-        return 'commercial'
-    elif b_type in ['residential', 'house', 'apartments', 'dormitory']:
-        return 'residential'
-    else:
-        return 'residential' # Default for unlabelled
+    building_use = str(row.get('building:use', '')).lower()
+    building = str(row.get('building', '')).lower()
+    landuse = str(row.get('landuse', '')).lower()
 
-def fetch_buildings(place_name, min_x, max_x, min_y, max_y, world_size):
+    if row.get('house', '') != '' or row.get('residential', '') != '':
+        return 'residential'
+    elif row.get('shop', '') != '':
+        return 'commercial'
+    elif row.get('education', '') != '' or row.get('place_of_worship', '') != '':
+        return 'special'
+    
+    fields = [amenity, building_use, building, landuse]
+
+    for field in fields:
+        if not field:
+            continue 
+
+        category = CATEGORY_MAP.get(field)
+        if category:
+            return category
+
+    return 'miscellaneous'
+    
+# Categorize each building
+# def categorize_building(row):
+#     print(row)
+#     amenity = str(row.get('amenity', '')).lower()
+#     b_type = str(row.get('building', '')).lower()
+
+
+    
+#     if 'school' in amenity or 'college' in amenity or 'university' in amenity:
+#         return 'school'
+#     elif 'hospital' in amenity or 'clinic' in amenity:
+#         return 'special' # Special zones
+#     elif b_type in ['retail', 'commercial', 'supermarket', 'office']:
+#         return 'commercial'
+#     elif b_type in ['residential', 'house', 'apartments', 'dormitory']:
+#         return 'residential'
+#     else:
+#         return 'residential' # Default for unlabelled
+
+def fetch_nodes(place_name, min_x, max_x, min_y, max_y, world_size):
     # Step 2:
     # Fetch buildings and get their 'tags'
     print("Fetching buildings.")
-    tags = {'building': True, 'amenity': ['school', 'college', 'university', 'hospital', 'clinic'], 'highway':'crossing'}
+    # tags = {'building': True, 'amenity': ['school', 'college', 'university', 'hospital', 'clinic'], 'highway':'crossing'}
+    tags = {
+        "building": True,
+        "amenity": True,
+        "shop": True,
+        "office": True,
+        "landuse": True,
+        "public_transport": True,
+        "highway": True
+    }
     buildings = ox.features_from_place(place_name, tags=tags)
 
     # Step 5:
@@ -92,6 +159,9 @@ def fetch_buildings(place_name, min_x, max_x, min_y, max_y, world_size):
     buildings['centroid'] = buildings.geometry.centroid
     buildings['x'] = buildings['centroid'].x
     buildings['y'] = buildings['centroid'].y
+
+    buildings.to_csv('raw_buildings.csv', index=False)
+    print(buildings)
 
     buildings['category'] = buildings.apply(categorize_building, axis=1)
 
